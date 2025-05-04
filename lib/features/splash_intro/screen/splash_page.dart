24c0be331dffe26368e470e1bc1f32e11441f0dc
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-05-05 01:23:03 +0300

    finish splash page

---
+++ lib/features/splash_intro/screen/splash_page.dart
@@ -0,0 +1,128 @@
+import 'package:doctor_booking1/constant/my_colours.dart';
+import 'package:doctor_booking1/constant/my_strings.dart';
+import 'package:doctor_booking1/features/splash_intro/screen/intro_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _logoAnimation;
+  late Animation<Offset> _textAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(
+        seconds: 4,
+      ),
+      vsync: this,
+    );
+
+    _logoAnimation = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(-0.25, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
+      ),
+    );
+
+    _textAnimation = Tween<Offset>(
+      begin: const Offset(1.5, 0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.2, 0.8, curve: Curves.easeOut),
+      ),
+    );
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.0, 0.5),
+      ),
+    );
+
+    _controller.forward().whenComplete(() async {
+      await Future.delayed(
+        const Duration(seconds: 1),
+      );
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+            transitionDuration: const Duration(
+              milliseconds: 600,
+            ),
+            pageBuilder: (context, animation, secondaryAnimation) =>
+                const IntroPage(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            },
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kColor,
+      body: Center(
+        child: FadeTransition(
+          opacity: _opacityAnimation,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlideTransition(
+                position: _logoAnimation,
+                child: Icon(
+                  kIcon,
+                  size: 80,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(width: 8),
+              SlideTransition(
+                position: _textAnimation,
+                child: const Text(
+                  'Care Connect',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
