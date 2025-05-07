9d7f950cc9766ab0f9e5e156711ba2cc4661fcc3
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-05-08 02:02:33 +0300

    finish auth repository

---
+++ lib/features/auth/data/repository/auth_repo.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:dartz/dartz.dart';
+import 'package:doctor_booking1/features/auth/data/datasource/remote_data_source.dart';
+import 'package:doctor_booking1/features/auth/data/models/login_requast.dart';
+import 'package:doctor_booking1/features/auth/data/models/login_response.dart';
+import 'package:doctor_booking1/features/auth/data/models/sign_up_request.dart';
+
+class AuthRepo {
+  final LoginData loginDataSource;
+  final SignUpData signUpDataSource;
+
+  AuthRepo({
+    required this.loginDataSource,
+    required this.signUpDataSource,
+  });
+
+  Future<Either<String, LoginResponse>> loginUser(LoginRequast params) async {
+    try {
+      final response = await loginDataSource.loginUser(params);
+      return Right(response);
+    } on TimeoutException catch (e) {
+      return Left(e.message ?? 'Timeout occurred');
+    } on Exception catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, SignUpRequest>> signUpUser(SignUpRequest params) async {
+    try {
+      final response = await signUpDataSource.signUpUser(params);
+      return Right(response);
+    } on TimeoutException catch (e) {
+      return Left(e.message ?? 'Timeout occurred');
+    } on Exception catch (e) {
+      return Left(e.toString());
+    }
+  }
+}
