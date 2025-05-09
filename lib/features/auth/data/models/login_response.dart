90ffcae536ee45e426ba723ff14df7d5380027a1
mohammedomar6 <80421895+mohammedomar6@users.noreply.github.com>
2025-05-09 20:01:36 +0300

    edit model login_response.dart

--- lib/features/auth/data/models/login_response.dart
+++ lib/features/auth/data/models/login_response.dart
@@ -2,6 +2,7 @@
 //
 //     final loginResponse = loginResponseFromJson(jsonString);
 
+import 'package:meta/meta.dart';
 import 'dart:convert';
 
 LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
@@ -10,88 +11,76 @@ String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
 
 class LoginResponse {
   final String status;
+  final String token;
   final User user;
-  final String tokens;
 
   LoginResponse({
     required this.status,
+    required this.token,
     required this.user,
-    required this.tokens,
   });
 
   LoginResponse copyWith({
     String? status,
+    String? token,
     User? user,
-    String? tokens,
   }) =>
       LoginResponse(
         status: status ?? this.status,
+        token: token ?? this.token,
         user: user ?? this.user,
-        tokens: tokens ?? this.tokens,
       );
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
     status: json["status"],
+    token: json["token"],
     user: User.fromJson(json["user"]),
-    tokens: json["tokens"],
   );
 
   Map<String, dynamic> toJson() => {
     "status": status,
+    "token": token,
     "user": user.toJson(),
-    "tokens": tokens,
   };
 }
 
 class User {
-  final String id;
   final String email;
-  final String name;
   final String role;
-  final String photo;
-  final String active;
+  final String createdAt;
+  final String id;
 
   User({
-    required this.id,
     required this.email,
-    required this.name,
     required this.role,
-    required this.photo,
-    required this.active,
+    required this.createdAt,
+    required this.id,
   });
 
   User copyWith({
-    String? id,
     String? email,
-    String? name,
     String? role,
-    String? photo,
-    String? active,
+    String? createdAt,
+    String? id,
   }) =>
       User(
-        id: id ?? this.id,
         email: email ?? this.email,
-        name: name ?? this.name,
         role: role ?? this.role,
-        photo: photo ?? this.photo,
-        active: active ?? this.active,
+        createdAt: createdAt ?? this.createdAt,
+        id: id ?? this.id,
       );
 
   factory User.fromJson(Map<String, dynamic> json) => User(
-    id: json["id"],
     email: json["email"],
-    name: json["name"],
     role: json["role"],
-    photo: json["photo"],
-    active: json["active"],
+    createdAt: json["createdAt"],
+    id: json["_id"],
   );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
     "email": email,
-    "name": name,
     "role": role,
-    "photo": photo,
-    "active": active,
+    "createdAt": createdAt,
+    "_id": id,
   };
 }
